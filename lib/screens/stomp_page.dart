c00a4badc3976191edadc1f94511a0c01a23e1cb
Siros Supavita <siros.supavita@gmail.com>
2023-11-21 15:54:03 +0700

    - Refactor StompButton class to a separated file - Added support for disabled WAH button when expression pedal is not   assigned to control WAH.

--- lib/screens/stomp_page.dart
+++ lib/screens/stomp_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'prorgam_scaffold.dart';
+import 'stomp_button.dart';
 import 'tempo_button.dart';
 import '/models/device.dart';
 
@@ -41,62 +42,5 @@ class StompPage extends StatelessWidget {
         ),
       );
     });
-  }
-}
-
-class StompButton extends StatelessWidget {
-  const StompButton(this.effectCategoryId, {super.key});
-
-  final String effectCategoryId;
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<DeviceModel>(
-      builder: (context, device, child) {
-        EffectBlock? effectBlock = device.getEffectBlock(effectCategoryId);
-        if (effectBlock == null) {
-          return const Text('');
-        }
-        Color blockColor = effectBlock.definition.category.color;
-
-        Color backgroundColor = device.isEffectOn(effectBlock)
-            ? blockColor
-            : Theme.of(context).colorScheme.background;
-        Color textColor = device.isEffectOn(effectBlock)
-            ? Theme.of(context).colorScheme.background
-            : blockColor;
-
-        return GestureDetector(
-            onTap: () => {device.toggleEffectBlock(effectCategoryId)},
-            child: Container(
-              padding: const EdgeInsets.all(5),
-              child: Container(
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    color: backgroundColor,
-                    border: Border.all(
-                      color: blockColor,
-                      width: 4,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      effectBlock.definition.name,
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                            color: textColor,
-                            decoration: effectBlock.isParallel
-                                ? TextDecoration.underline
-                                : TextDecoration.none,
-                            decorationStyle: TextDecorationStyle.double,
-                            decorationThickness: 2,
-                            decorationColor: textColor,
-                          ),
-                    ),
-                  )),
-            ));
-      },
-    );
   }
 }
